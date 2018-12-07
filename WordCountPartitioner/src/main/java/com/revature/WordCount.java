08a2e950c6a0c8318a03d9213e7de588d5ed64ff
cloudera <cloudera@quickstart.cloudera>
2018-12-07 10:49:31 -0800

    First push.

---
+++ WordCountPartitioner/src/main/java/com/revature/WordCount.java
@@ -0,0 +1,13 @@
+package com.revature;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.util.ToolRunner;
+
+public class WordCount {
+
+	public static void main(String[] args) throws Exception {
+		
+		int exitCode = ToolRunner.run(new Configuration(), new WordCountRunner(), args);
+		System.exit(exitCode);
+	}
+}
